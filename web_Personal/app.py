385fedd4cdd905e7ecd4b67d94a01d393c197381
Erik <erik.f.m.0525@gmail.com>
2022-12-02 17:41:13 -0600

    Edición 18 RegisterForm Parte 1

--- web_Personal/app.py
+++ web_Personal/app.py
@@ -1,7 +1,10 @@
 #from crypt import methods
 from flask import Flask, redirect ,render_template, request, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms import (StringField, PasswordField, 
+                    SubmitField, EmailField, 
+                    IntegerField, RadioField,
+                    SelectField, TextAreaField)
 from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
@@ -46,6 +49,17 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Ingresar')
 
+
+class RegisterForm(FlaskForm):
+    name = StringField('Nombre')
+    last_name = StringField('Apellidos')
+    email = EmailField('Correo')
+    password = PasswordField('Contraseña')
+    phone = IntegerField('Teléfono')
+    is_married = RadioField('Estadi civil', choices=[('True', 'Casado'), ('False', 'Soltero')])
+    gender = SelectField('Genero', choices=[('male', 'Masculino'), ('female', 'Femenino'), ('other', 'Otro')])
+    submit = SubmitField('Registrar')
+
 ######## Rutas ##########
 
 @app.route('/auth/login', methods=['GET, POST'])
@@ -64,19 +78,12 @@ def login():
 
 @app.route('/auth/register')
 def register():
-    return render_template('auth/register.html')
 
-@app.route('/welcome', methods=['GET', 'POST'])
-def welcome(form):
+    form = RegisterForm()
 
-    if form.validate_on_submit():       
-         email = form.email.data
-         password = form.password.data
-        
+    return render_template('auth/register.html', form=form)
 
-    return render_template('admin/index.html', email=email)
 
-    return redirect(url_for('login') )
 
 @app.errorhandler(404)
 def page_error_not_found(e):
